32dc0a47d1163cfda9e302fd8b37cd061c278583
Tetsuro Kitahara <kitahara@kthrlab.jp>
2024-09-13 09:36:54 +0900

    added JamSktechEngineSimple and NoteSeqGenerator; edited config.txt etc.

--- jamsketch.groovy
+++ jamsketch.groovy
@@ -390,8 +390,8 @@ class JamSketch extends SimplePianoRoll {
   }
 
 }
-// JamSketch.CFG = evaluate(new File("./config.txt"))
-JamSketch.CFG = evaluate(new File("./config_guided.txt"))
+JamSketch.CFG = evaluate(new File("./config.txt"))
+//JamSketch.CFG = evaluate(new File("./config_guided.txt"))
 JamSketch.start("JamSketch")
 // JamSketch.main("JamSketch", ["--external"] as String[])
   
