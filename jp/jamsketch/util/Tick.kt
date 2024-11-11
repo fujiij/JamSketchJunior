2070506336a80ece1e8bded783f5afa29dedc810
fujiij <32182215+fujiij@users.noreply.github.com>
2024-11-11 14:41:22 +0900

    Migrate from Groovy to Kotlin

--- jp/jamsketch/util/Tick.kt
+++ jp/jamsketch/util/Tick.kt
@@ -1,5 +1,5 @@
 package jp.jamsketch.util
 
 interface Tick {
-    public void tick();
+    fun tick()
 }
--- jp/jamsketch/view/IDisplay.kt
+++ jp/jamsketch/view/IDisplay.kt
@@ -3,6 +3,5 @@ package jp.jamsketch.view
 import jp.crestmuse.cmx.processing.gui.SimplePianoRoll
 
 interface IDisplay {
-
-    void display(SimplePianoRoll scene);
+    fun display(scene: SimplePianoRoll?)
 }
