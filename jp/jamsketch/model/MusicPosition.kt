7d15ae4ecd05c218f99043bce5b1fb00c26d6650
fujiij <32182215+fujiij@users.noreply.github.com>
2024-11-11 13:13:02 +0900

    Migrate from Groovy to Kotlin

---
+++ jp/jamsketch/model/MusicPosition.kt
@@ -0,0 +1,22 @@
+package jp.jamsketch.model
+
+class MusicPosition {
+    constructor(measure: Int, tick: Int) {
+        this.measure = measure
+        this.tick = tick
+    }
+
+    constructor(mouseX: Double, mouseY: Double)
+
+    override fun equals(obj: Any?): Boolean {
+        if (obj is MusicPosition) {
+            val mp = obj
+            return mp.tick == tick && mp.measure == measure
+        } else {
+            return false
+        }
+    }
+
+    private var measure = 0
+    private var tick = 0
+}
